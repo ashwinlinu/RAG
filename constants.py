d460435a24077a8561a3beb4becd37cc3b919cff
Ashwin Linu <ashwinl@terawe.com>
2026-04-28 16:50:20 +0530

    changed Native openAi to azureOpenAi

--- constants.py
+++ constants.py
@@ -8,3 +8,6 @@ load_dotenv()
 class ENV_VARIABLES:
     OPEN_AI_API_KEY = os.environ.get("OPENAI_API_KEY")
     OPEN_AI_MODEL_NAME = os.environ.get("OPENAI_GPT_MODEL_NAME")
+    AZURE_OPENAI_API_VERSION = os.environ.get("AZURE_OPENAI_API_VERSION")
+    AZURE_OPENAI_ENDPOINT = os.environ.get("AZURE_OPENAI_ENDPOINT")
+    AZURE_OPENAI_API_KEY = os.environ.get("AZURE_OPENAI_API_KEY")
